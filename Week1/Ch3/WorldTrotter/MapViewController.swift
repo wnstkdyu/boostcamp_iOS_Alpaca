ab014443377e8b658cb41a301c9633e249a81e6e
wnstkdyu <wnstkdyu@gmail.com>
2017-07-01 14:04:40 +0900

    6장 중간 저장

--- Week1/Ch3/WorldTrotter/MapViewController.swift
+++ Week1/Ch3/WorldTrotter/MapViewController.swift
@@ -7,8 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // 지도 뷰 생성
+        mapView = MKMapView()
+        
+        // 지도 뷰를 이 뷰 컨트롤러의 view로 설정
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        
+        // whiteColor().colorWithAlphaComponent(0.5)에서 변경
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        // segmentedControl 뷰를 상위 뷰의 위, leading, trailing이 같게 제약조건을 형성
+        // 변경점 있음
+        //let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        /* 상태바 밑에 오도록 topLayoutGuide의 bottomAnchor에 segmentedControl의 topAnchor를 맞춘다 */
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        /*
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        */
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        // 활성화
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
