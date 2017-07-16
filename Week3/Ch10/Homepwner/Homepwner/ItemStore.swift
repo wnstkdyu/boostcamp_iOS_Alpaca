7bb676a4f917d3def81016bc85aa15a6610e951a
wnstkdyu <wnstkdyu@gmail.com>
2017-07-16 16:36:53 +0900

    Ch10 메달 과제 전까지 완료

--- Week3/Ch10/Homepwner/Homepwner/ItemStore.swift
+++ Week3/Ch10/Homepwner/Homepwner/ItemStore.swift
@@ -12,12 +12,12 @@ class ItemStore {
     // 아이템들을 저장할 배열
     var allItems = [Item]()
     
-    // 5개의 임의의 물품을 추가하는 지정 이니셜라이저
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
+//    // 5개의 임의의 물품을 추가하는 지정 이니셜라이저
+//    init() {
+//        for _ in 0..<5 {
+//            createItem()
+//        }
+//    }
     
     // 새 물품을 만들고 반환하는 메서드 구현
     func createItem() -> Item {
@@ -26,5 +26,28 @@ class ItemStore {
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    // 특정 항목을 제거하는 메서드 구현
+    func removeItem(item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    // 배열의 항목 순서를 변경하는 메서드 구현
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // 다시 삽입할 수 있도록 이동할 객체의 레퍼런스를 얻는다.
+        let movedItem = allItems[fromIndex]
+        
+        // 항목을 배열에서 제거한다.
+        allItems.remove(at: fromIndex)
+        
+        // 항목을 배열에서 새 위치에 삽입한다.
+        allItems.insert(movedItem, at: toIndex)
     }
 }
--- Week3/Ch10/Homepwner/Homepwner/ItemsViewController.swift
+++ Week3/Ch10/Homepwner/Homepwner/ItemsViewController.swift
@@ -47,4 +47,85 @@ class ItemsViewController: UITableViewController {
         
         return cell
     }
+    
+    // 테이블 행 삭제하기
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        // 테이블 뷰가 삭제 명령의 적용을 요청하면
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            // alert의 메시지 작성
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            // actionSheet의 action 추가
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                                             handler: { (action) -> Void in
+                                                // 저장소에서 그 항목을 제거한다.
+                                                self.itemStore.removeItem(item: item)
+                                                // 또한 애니메이션과 함께 테이블 뷰에서 그 행을 제거한다.
+                                                tableView.deleteRows(at: [indexPath], with: .automatic)})
+            ac.addAction(deleteAction)
+            
+            // 알림창 컨트롤러를 표시한다.
+            present(ac, animated: true, completion: nil)
+            
+//            // 저장소에서 그 항목을 제거한다.
+//            itemStore.removeItem(item: item)
+//            
+//            // 또한 애니메이션과 함께 테이블 뷰에서 그 행을 제거한다.
+//            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    // 테이블 행 옮기기
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // 모델을 업데이트한다.
+        itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
+    
+    // 테이블 헤더 뷰 구현
+    @IBAction func addNewItem(sender: AnyObject) {
+//        // 0번 섹션, 마지막 행의 인덱스 패스를 만든다.
+//        let lastRow = tableView.numberOfRows(inSection: 0)
+//        let indexPath = IndexPath(row: lastRow, section: 0)
+//        
+//        // 테이블에 새로운 행을 삽입한다.
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        // 새 물품을 만들고 그것을 저장소에 추가한다.
+        let newItem = itemStore.createItem()
+        
+        // 배열 안에서 이 항목의 위치를 계산한다.
+        guard let index = itemStore.allItems.index(of: newItem) else {
+            fatalError()
+        }
+        let indexPath = IndexPath(row: index, section: 0)
+        
+        // 테이블에 새로운 행을 삽입한다.
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        // 현재 편집 모드이면
+        if isEditing {
+            // 사용자에게 상태를 알리기 위해 버튼의 텍스트를 변경한다.
+            sender.setTitle("Edit", for: .normal)
+            
+            // 편집 모드를 끈다.
+            setEditing(false, animated: true)
+        } else {
+            // 사용자에게 상태를 알리기 위해 버튼의 텍스트를 변경한다.
+            sender.setTitle("Done", for: .normal)
+            
+            // 편집 모드로 들어간다.
+            setEditing(true, animated: true)
+        }
+        
+    }
 }
