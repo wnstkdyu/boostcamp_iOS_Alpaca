c9602be7376754407f2ae1795eae23338ede8a2f
wnstkdyu <wnstkdyu@gmail.com>
2017-07-16 15:08:49 +0900

    Homepwner 복사 후 Ch10 시작

---
+++ Week3/Ch10/Homepwner/Homepwner/Item.swift
@@ -0,0 +1,49 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Alpaca on 2017. 7. 15..
+//  Copyright © 2017년 Alpaca. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    // 네개의 프로퍼티 선언
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: NSDate
+    
+    // 지정 이니셜라이저
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = NSDate()
+        
+        super.init()
+    }
+    
+    // 컨비니언스 이니셜라이저
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bears", "Spork", "Mac"]
+            
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(idx)]
+            
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = NSUUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+}
