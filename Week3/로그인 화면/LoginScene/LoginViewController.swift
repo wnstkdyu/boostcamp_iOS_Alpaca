5d3375859c4333439dc8997a4a898604e7537479
wnstkdyu <wnstkdyu@gmail.com>
2017-07-17 16:36:10 +0900

    개인 과제 완료

--- Week3/로그인 화면/LoginScene/LoginViewController.swift
+++ Week3/로그인 화면/LoginScene/LoginViewController.swift
@@ -12,6 +12,7 @@ import FacebookLogin
 class LoginViewController: UIViewController, UITextFieldDelegate {
     
     var myButton: MyButton!
+    var disableBtn: UIButton!
     
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var pwTextField: UITextField!
@@ -29,7 +30,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         loginButton.center.x = view.center.x
         loginButton.center.y = view.center.y.adding(100.0)
         
-        // MyButton 추가
+        
+        /* myButton */
+        // myButton 인스턴스 생성
         myButton = MyButton()
         
         view.addSubview(myButton)
@@ -42,8 +45,34 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         myButton.heightAnchor.constraint(equalToConstant: 100.0).isActive = true
         myButton.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
         myButton.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
-
+        
+        
+        /* disableBtn */
+        // disableBtn 인스턴스 생성
+        disableBtn = UIButton()
+        
+        view.addSubview(disableBtn)
+        
+        // disableBtn 설정
+        disableBtn.setTitle("Disable the button", for: .normal)
+        disableBtn.setTitle("Enable the button", for: .selected)
+        
+        disableBtn.setTitleColor(UIColor.black, for: .normal)
+        disableBtn.setTitleColor(UIColor.black, for: .selected)
+        disableBtn.titleLabel?.textAlignment = .center
+        disableBtn.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        // disableBtn 제약조건 설정
+        disableBtn.translatesAutoresizingMaskIntoConstraints = false
+        disableBtn.topAnchor.constraint(equalTo: myButton.bottomAnchor).isActive = true
+        disableBtn.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        disableBtn.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        disableBtn.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        
+        // disableBtn func 설정
+        disableBtn.addTarget(self, action: #selector (disableMyBtn(_:)), for: .touchUpInside)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -64,7 +93,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    
     @IBAction func signIn(_ sender: Any) {
         print("touch up inside - sign in")
         if let id = idTextField.text,
@@ -81,5 +109,21 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         print("touch up inside - sign up")
     }
     
+    func disableMyBtn(_ sender: UIButton) {
+        // sender의 상태에 따라 다르게
+        if sender.isHighlighted {
+            if sender.isSelected {
+                myButton.addGestureRecognizer(myButton.tapGestureRecognizer)
+                myButton.addGestureRecognizer(myButton.longPressGestureRecognizer)
+                
+                sender.isSelected = false
+            } else if sender.isSelected == false {
+                myButton.removeGestureRecognizer(myButton.tapGestureRecognizer)
+                myButton.removeGestureRecognizer(myButton.longPressGestureRecognizer)
+                
+                sender.isSelected = true
+            }
+        }
+    }
 }
 
--- Week3/로그인 화면/LoginScene/MyButton.swift
+++ Week3/로그인 화면/LoginScene/MyButton.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class MyButton: UIView {
+class MyButton: UIView, UIGestureRecognizerDelegate {
     
     var stateLabel: UILabel!
     var backGroundImageView: UIImageView!
     var tapGestureRecognizer: UITapGestureRecognizer!
+    var longPressGestureRecognizer: UILongPressGestureRecognizer!
+    var controlState: UIControlState!
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,10 +24,12 @@ class MyButton: UIView {
         self.backgroundColor = UIColor.darkGray
         print("MyButton Load")
         
+        // button으로써 누를 수 있도록
+        self.isUserInteractionEnabled = true
         
         /* stateLabel */
         // stateLabel 인스턴스 생성
-        stateLabel = UILabel()
+        stateLabel = UILabel(frame: CGRect.init())
         self.addSubview(stateLabel)
         
         // stateLabel의 텍스트 설정
@@ -37,7 +42,7 @@ class MyButton: UIView {
         stateLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         stateLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         stateLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        stateLabel.trailingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         
         
         /* backGroundImageView */
@@ -57,18 +62,93 @@ class MyButton: UIView {
         backGroundImageView.sizeToFit()
         
         
-        /* tapGestureRecognizer */
-        // tapGestureRecognizer 인스턴스 생성
+        /* gestureRecognizer */
+        // gestureRecognizer 인스턴스 생성
         tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(actionWhenTapping(sender:)))
+        longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(actionWhenLongPressing(sender:)))
+        
+        // gestureRecognizer delegate를 myButton으로 설정
+        tapGestureRecognizer.delegate = self
+        longPressGestureRecognizer.delegate = self
+        
+        longPressGestureRecognizer.minimumPressDuration = 0.01
+        
+        self.addGestureRecognizer(tapGestureRecognizer)
+        self.addGestureRecognizer(longPressGestureRecognizer)
+        
+        
+        /* controlState */
+        // controlState의 상태 초기화
+        controlState = UIControlState.normal
+        stateLabel.text = "normal"
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // 탭시 변화하는 메서드 구현
+    // 탭 시 변화하는 메서드 구현
     func actionWhenTapping(sender: UITapGestureRecognizer) {
         // 버튼의 상태에 따라 달라짐
+        switch controlState {
+        case UIControlState.normal:
+            controlState = UIControlState.selected
+            stateLabel.text = "selected"
+            stateLabel.textColor = UIColor.green
+        case UIControlState.selected:
+            controlState = UIControlState.normal
+            stateLabel.text = "normal"
+            stateLabel.textColor = UIColor.black
+        default:
+            break
+        }
+    }
+    
+    // 롱프레스 시 변화하는 메서드 구현
+    func actionWhenLongPressing(sender: UILongPressGestureRecognizer) {
+        // normal에서 누르고 있을 경우
+        if sender.state == .began {
+            switch controlState {
+                // normal -> highlight1
+            case UIControlState.normal:
+                controlState = UIControlState.highlighted
+                stateLabel.text = "highlighted1"
+                stateLabel.textColor = UIColor.white
+                
+                backGroundImageView.alpha = 0.8
+                // selected -> highlight2
+            case UIControlState.selected:
+                controlState = UIControlState.highlighted
+                stateLabel.text = "highlighted2"
+                stateLabel.textColor = UIColor.red
+                
+                backGroundImageView.alpha = 0.8
+            default:
+                break
+            }
+        } else if sender.state == .ended {
+            guard let labelText = stateLabel.text else {
+                fatalError()
+            }
+            switch labelText {
+                // highlighted1 -> selected
+            case "highlighted1":
+                controlState = UIControlState.selected
+                stateLabel.text = "selected"
+                stateLabel.textColor = UIColor.green
+                
+                backGroundImageView.alpha = 0.3
+                // highlighted2 -> normal
+            case "highlighted2":
+                controlState = UIControlState.normal
+                stateLabel.text = "normal"
+                stateLabel.textColor = UIColor.black
+                
+                backGroundImageView.alpha = 0.3
+            default:
+                break
+            }
+        }
     }
     
 }
