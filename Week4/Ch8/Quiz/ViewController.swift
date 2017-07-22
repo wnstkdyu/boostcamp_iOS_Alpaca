eb69baece234d8cf8359b341833a4792d24ee159
wnstkdyu <wnstkdyu@gmail.com>
2017-07-23 08:46:57 +0900

    Quiz 프로젝트 복사 후 Ch8 시작

---
+++ Week4/Ch8/Quiz/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Alpaca on 2017. 6. 30..
+//  Copyright © 2017년 Alpaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        /*
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        */
+        currentQuestionIndex = (currentQuestionIndex == questions.count - 1) ? 0 : currentQuestionIndex + 1
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+}
+
