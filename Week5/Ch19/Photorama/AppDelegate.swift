c4952f138abf724caa1444cd956a3024eca699d1
wnstkdyu <wnstkdyu@gmail.com>
2017-07-31 15:17:56 +0900

    19장 완료

--- Week5/Ch19/Photorama/AppDelegate.swift
+++ Week5/Ch19/Photorama/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        guard let rootViewController = window?.rootViewController as? UINavigationController else {
+            return true
+        }
+        guard let photosViewController = rootViewController.topViewController as? PhotosViewController else {
+            return true
+        }
+        photosViewController.store = PhotoStore()
         return true
     }
 
